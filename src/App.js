2e213dafd2a9fc7fecaa7391675665d5de737422
Jay Chen <walkccray@gmail.com>
2019-10-20 18:24:02 +0800

    Add Palette.js and pass props seedPalette[0]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Palette from './components/Palette';
+import seedPalettes from './config/seedPalettes';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Palette {...seedPalettes[0]} />
+    </div>
+  );
 }
 
 export default App;

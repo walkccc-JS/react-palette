899677c8e77cd41e16a4df5f77db6f329ad2ad18
Jay Chen <walkccray@gmail.com>
2019-10-24 19:13:24 +0800

    Refactor PaletteList w/ Hooks

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { withStyles } from '@material-ui/styles';
 import MiniPalette from './MiniPalette';
+import { withStyles } from '@material-ui/styles';
 import styles from './styles/PaletteListStyles';
 
-class PaletteList extends Component {
-  goToPalette = id => {
-    this.props.history.push(`/palette/${id}`);
+function PaletteList(props) {
+  const { classes, palettes } = props;
+
+  const goToPalette = id => {
+    props.history.push(`/palette/${id}`);
   };
 
-  render() {
-    const { classes, palettes } = this.props;
-
-    return (
-      <div className={classes.root}>
-        <div className={classes.container}>
-          <nav className={classes.nav}>
-            <h1>React Palette</h1>
-            <Link to="/palette/new">Create palette</Link>
-          </nav>
-          <div className={classes.palettes}>
-            {palettes.map(palette => (
-              <MiniPalette
-                {...palette}
-                handleClick={() => this.goToPalette(palette.id)}
-              />
-            ))}
-          </div>
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Palette</h1>
+          <Link to="/palette/new">Create palette</Link>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map(palette => (
+            <MiniPalette
+              {...palette}
+              handleClick={() => goToPalette(palette.id)}
+            />
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default withStyles(styles)(PaletteList);

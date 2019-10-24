0ea1cbcadacb11094274bd081248a127d009e77c
Jay Chen <walkccray@gmail.com>
2019-10-24 19:08:25 +0800

    Refactor Navbar w/ Hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Slider from 'rc-slider';
 import { Select } from '@material-ui/core';
@@ -10,74 +10,70 @@ import { withStyles } from '@material-ui/styles';
 import 'rc-slider/assets/index.css';
 import styles from './styles/NavbarStyles';
 
-class Navbar extends Component {
-  state = { format: 'hex', open: false };
+function Navbar(props) {
+  const [format, setFormat] = useState('hex');
+  const [open, setOpen] = useState(false);
+  const { classes, level, showingAllColors, changeLevel, changeFormat } = props;
 
-  handleFormatChange = e => {
-    this.setState({ format: e.target.value, open: true });
-    this.props.changeFormat(e.target.value);
+  const handleFormatChange = e => {
+    setFormat(e.target.value);
+    setOpen(true);
+    changeFormat(e.target.value);
   };
 
-  closeSnackbar = () => {
-    this.setState({ open: false });
+  const closeSnackbar = () => {
+    setOpen(false);
   };
 
-  render() {
-    const { classes, level, showingAllColors, changeLevel } = this.props;
-    const { format } = this.state;
-
-    return (
-      <header className={classes.Navbar}>
-        <div className={classes.logo}>
-          <Link to="/">react palette</Link>
+  return (
+    <header className={classes.Navbar}>
+      <div className={classes.logo}>
+        <Link to="/">react palette</Link>
+      </div>
+      {showingAllColors && (
+        <div>
+          <span>Level: {level}</span>
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={changeLevel}
+            className={classes.slider}
+          />
         </div>
-        {showingAllColors && (
-          <div>
-            <span>Level: {level}</span>
-            <Slider
-              defaultValue={level}
-              min={100}
-              max={900}
-              step={100}
-              onAfterChange={changeLevel}
-              className={classes.slider}
-            />
-          </div>
-        )}
-        <div className={classes.selectContainer}>
-          <Select value={format} onChange={this.handleFormatChange}>
-            <MenuItem value="hex">HEX - #ffffff</MenuItem>
-            <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
-            <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
-          </Select>
-        </div>
-        <Snackbar
-          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-          open={this.state.open}
-          autoHideDuration={3000}
-          message={
-            <span id="message-id">
-              Format changed to {format.toUpperCase()}
-            </span>
-          }
-          ContentProps={{
-            'aria-describedby': 'message-id'
-          }}
-          onClose={this.closeSnackbar}
-          action={[
-            <IconButton
-              onClick={this.closeSnackbar}
-              color="inherit"
-              key="close"
-              aria-label="close"
-            >
-              <CloseIcon />
-            </IconButton>
-          ]}
-        ></Snackbar>
-      </header>
-    );
-  }
+      )}
+      <div className={classes.selectContainer}>
+        <Select value={format} onChange={handleFormatChange}>
+          <MenuItem value="hex">HEX - #ffffff</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
+        </Select>
+      </div>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        open={open}
+        autoHideDuration={3000}
+        message={
+          <span id="message-id">Format changed to {format.toUpperCase()}</span>
+        }
+        ContentProps={{
+          'aria-describedby': 'message-id'
+        }}
+        onClose={closeSnackbar}
+        action={[
+          <IconButton
+            onClick={closeSnackbar}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+        ]}
+      ></Snackbar>
+    </header>
+  );
 }
 
 export default withStyles(styles)(Navbar);

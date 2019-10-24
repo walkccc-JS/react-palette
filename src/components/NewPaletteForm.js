bd6d9214757e3fb9aefcc7a596856421a31a4cf7
Jay Chen <walkccray@gmail.com>
2019-10-24 16:58:36 +0800

    Add ChromePicker and Buttons to Drawer

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -10,8 +10,10 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Button from '@material-ui/core/Button';
+import ChromePicker from 'react-color';
 
-const drawerWidth = 240;
+const drawerWidth = 300;
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -120,6 +122,20 @@ function NewPaletteForm() {
           </IconButton>
         </div>
         <Divider />
+        <Typography variant="h4">Design your palette</Typography>
+        <Button variant="contained" color="secondary">
+          Clear palette
+        </Button>
+        <Button variant="contained" color="primary">
+          Random color
+        </Button>
+        <ChromePicker
+          color="purple"
+          onChangeComplete={newColor => console.log(newColor)}
+        />
+        <Button variant="contained" color="primary">
+          Add color
+        </Button>
       </Drawer>
       <main
         className={clsx(classes.content, {

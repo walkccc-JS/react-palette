1787d327f7dfdf507a2116ac71d8b4ff7b147ed0
Jay Chen <walkccray@gmail.com>
2019-10-26 04:11:42 +0800

    Clean up PaletteMetaForm

--- src/components/PaletteMetaForm.js
+++ src/components/PaletteMetaForm.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { useInput } from './hooks/InputHook';
 import { Picker } from 'emoji-mart';
 import 'emoji-mart/css/emoji-mart.css';
@@ -39,8 +39,8 @@ function PaletteMetaForm({ palettes, handleSubmit, hideForm }) {
         <Picker title="Pick a Palette Emoji" onSelect={savePalette} />
       </Dialog>
       <Dialog
-        open={stage === 'form'}
         aria-labelledby="form-dialog-title"
+        open={stage === 'form'}
         onClose={hideForm}
       >
         <DialogTitle id="form-dialog-title">Choose a Palette Name</DialogTitle>
@@ -51,19 +51,19 @@ function PaletteMetaForm({ palettes, handleSubmit, hideForm }) {
               unique!
             </DialogContentText>
             <TextValidator
-              label="Palette Name"
               fullWidth
               margin="normal"
+              label="Palette Name"
               validators={['required', 'isPaletteNameUnique']}
               errorMessages={['Enter Palette Name', 'Name already used']}
               {...bindNewPaletteName}
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={hideForm} color="primary">
+            <Button color="primary" onClick={hideForm}>
               Cancel
             </Button>
-            <Button variant="contained" color="primary" type="submit">
+            <Button color="primary" variant="contained" type="submit">
               Save Palette
             </Button>
           </DialogActions>

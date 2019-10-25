74cda4ad7b4d38489e9ac9dc06a6b4eb8ca8637a
Jay Chen <walkccray@gmail.com>
2019-10-26 03:39:14 +0800

    Clean up DraggableColorBoxList

--- src/components/DraggableColorBoxList.js
+++ src/components/DraggableColorBoxList.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import DraggableColorBox from './DraggableColorBox';
 import { SortableContainer } from 'react-sortable-hoc';
+import DraggableColorBox from './DraggableColorBox';
 
 const DraggableColorBoxList = SortableContainer(({ colors, removeColor }) => {
   return (
     <div style={{ height: '100%' }}>
       {colors.map((color, index) => (
         <DraggableColorBox
+          index={index}
           color={color.color}
           name={color.name}
           key={color.name}
-          index={index}
           handleClick={() => removeColor(color.name)}
         />
       ))}

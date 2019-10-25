7b87cb467d4ce732b1629f8f53a88b187d91288c
Jay Chen <walkccray@gmail.com>
2019-10-26 05:38:00 +0800

    Updates

---
+++ precache-manifest.567c044829d6d2b2aa074366993ac594.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf533411d7b45a8383800a65c36d73af",
+    "url": "/react-palette/index.html"
+  },
+  {
+    "revision": "8b64ef6baaa7470f5eab",
+    "url": "/react-palette/static/css/2.5774dd83.chunk.css"
+  },
+  {
+    "revision": "7499ceb20cfb21463a51",
+    "url": "/react-palette/static/css/main.00907d78.chunk.css"
+  },
+  {
+    "revision": "8b64ef6baaa7470f5eab",
+    "url": "/react-palette/static/js/2.88338402.chunk.js"
+  },
+  {
+    "revision": "7499ceb20cfb21463a51",
+    "url": "/react-palette/static/js/main.d8fbc5c3.chunk.js"
+  },
+  {
+    "revision": "48f6cf1a6a24e4432992",
+    "url": "/react-palette/static/js/runtime-main.3a076bf5.js"
+  },
+  {
+    "revision": "74e82c654a8c2cc0a9d4b55cbaf466f5",
+    "url": "/react-palette/static/media/logo.74e82c65.svg"
+  }
+]);

35162c5be924b2926afb896eda25e3167177e2e8
Jay Chen <walkccray@gmail.com>
2019-10-26 03:21:17 +0800

    Clean up ColorBox

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,37 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { withStyles } from '@material-ui/styles';
+import { IconButton, Snackbar } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ViewComfyIcon from '@material-ui/icons/ViewComfy';
+import { withStyles } from '@material-ui/styles';
 import styles from './styles/ColorBoxStyles';
 
 function ColorBox({ classes, name, background, moreUrl, showingFullPalette }) {
   const [copied, setCopied] = useState(false);
 
-  const changeCopyState = () => {
-    setCopied(true);
-  };
+  const handleCopy = () => setCopied(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setCopied(false);
-    }, 1500);
-  }, [copied]);
+  const finishCopied = () => setCopied(false);
 
   return (
-    <CopyToClipboard text={background} onCopy={changeCopyState}>
+    <CopyToClipboard text={background} onCopy={handleCopy}>
       <div style={{ background }} className={classes.ColorBox}>
-        <div
-          style={{ background }}
-          className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}
-        />
-        <div
-          className={`${classes.copyMessage} ${copied && classes.showMessage}`}
-        >
-          <h1>copied!</h1>
-          <p className={classes.copyText}>{background}</p>
-        </div>
         <div>
           <div className={classes.boxContent}>
             <span className={classes.colorName}>{name}</span>
@@ -44,6 +30,24 @@ function ColorBox({ classes, name, background, moreUrl, showingFullPalette }) {
               </Link>
             )}
           </div>
+          <Snackbar
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+            autoHideDuration={1200}
+            ContentProps={{ 'aria-describedby': 'message-id' }}
+            message={<span id="message-id">{background} copied</span>}
+            open={copied}
+            onClose={finishCopied}
+            action={[
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                key="close"
+                onClick={finishCopied}
+              >
+                <CloseIcon />
+              </IconButton>
+            ]}
+          />
         </div>
       </div>
     </CopyToClipboard>
--- src/components/styles/ColorBoxStyles.js
+++ src/components/styles/ColorBoxStyles.js
@@ -24,18 +24,6 @@ export default {
       height: props => (props.showingFullPalette ? '5%' : '10%')
     }
   },
-  copyText: {
-    color: props =>
-      chroma(props.background).luminance() >= 0.5
-        ? 'rgba(0, 0, 0, 0.5)'
-        : 'white'
-  },
-  colorName: {
-    color: props =>
-      chroma(props.background).luminance() <= 0.08
-        ? 'white'
-        : 'rgba(0, 0, 0, 0.5)'
-  },
   buttons: {
     left: '40%',
     right: '40%',
@@ -84,58 +72,5 @@ export default {
     letterSpacing: '1px',
     position: 'absolute',
     textTransform: 'uppercase'
-  },
-  copyOverlay: {
-    width: '100%',
-    height: '100%',
-    opacity: '0',
-    transform: 'scale(0.1)',
-    transition: 'transform 0.6s ease-in-out',
-    zIndex: '0'
-  },
-  showOverlay: {
-    opacity: '1',
-    position: 'absolute',
-    transform: 'scale(50)',
-    zIndex: '10'
-  },
-  copyMessage: {
-    left: '0',
-    right: '0',
-    top: '0',
-    bottom: '0',
-    alignItems: 'center',
-    color: 'white',
-    display: 'flex',
-    flexDirection: 'column',
-    fontSize: '4rem',
-    justifyContent: 'center',
-    opacity: '0',
-    position: 'fixed',
-    transform: 'scale(0.1)',
-    '& h1': {
-      width: '100%',
-      marginBottom: '0',
-      padding: '1rem',
-      background: 'rgba(255, 255, 255, 0.2)',
-      fontWeight: '400',
-      textAlign: 'center',
-      textTransform: 'uppercase',
-      textShadow: '2px 2px black',
-      [sizes.down('xs')]: {
-        fontSize: '5rem'
-      }
-    },
-    '& p': {
-      fontWeight: '100',
-      fontSize: '2rem'
-    }
-  },
-  showMessage: {
-    opacity: '1',
-    transform: 'scale(1)',
-    transition: 'all 0.4s ease-in-out',
-    transitionDelay: '0.3s',
-    zIndex: '25'
   }
 };
